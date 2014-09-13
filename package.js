f9ed6f0e71462f418f973d1a5ef1e12b0bb06ee5
Mitar <mitar.git@tnode.com>
2014-09-12 23:05:13 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,22 +1,10 @@
 Package.describe({
   summary: "fs node.js module with fiber-enabled synchronous functions",
   version: '0.1.5',
-  name: 'peerlibrary:fs',
+  name: 'mrt:fs',
   git: 'https://github.com/peerlibrary/meteor-fs.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-  api.use(['peerlibrary:blocking@0.4.2', 'underscore', 'peerlibrary:assert@0.2.5'], 'server');
-
-  api.export('fs');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:fs', 'tinytest', 'test-helpers'], 'server');
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:fs@0.1.5');
 });
