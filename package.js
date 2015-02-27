6a20a3d0b638443ce84736df5f8cdfb6cfdd290e
Mitar <mitar.git@tnode.com>
2015-02-26 21:56:54 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "fs node.js module with fiber-enabled synchronous functions",
-  version: '0.1.6',
+  version: '0.1.7',
   name: 'peerlibrary:fs',
   git: 'https://github.com/peerlibrary/meteor-fs.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.0.2.1');
-  api.use(['peerlibrary:blocking@0.5.1', 'underscore', 'peerlibrary:assert@0.2.5'], 'server');
+  api.versionsFrom('METEOR@1.0.3.2');
+  api.use(['peerlibrary:blocking@0.5.2', 'underscore', 'peerlibrary:assert@0.2.5'], 'server');
 
   api.export('fs');
 
